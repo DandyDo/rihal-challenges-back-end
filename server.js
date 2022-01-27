97c9b22ded1ba62eaf8aec7bf0ef892ff8ecd36f
dandydo <alqasimtoon@gmail.com>
2022-01-27 21:50:09 +0400

    commented out an import that was causing heroku to crash

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config';
+// import 'dotenv/config';
 import cors from 'cors';
 import knex from 'knex';
 import { handleAddClass } from './controllers/classes/addClass.js';
